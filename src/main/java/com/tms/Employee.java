9a178d4e7c609ba5c523571fd97cebc077a4bbd1
Poviraevsergei <poviraevsergei@gmail.com>
2023-07-04 20:13:41 +0300

    Annotation bean creation

--- src/main/java/com/tms/Employee.java
+++ src/main/java/com/tms/Employee.java
@@ -1,5 +1,8 @@
 package com.tms;
 
+import org.springframework.stereotype.Component;
+
+@Component(value = "employee-bean") //разрешает спрингу управлять обьектами Employee
 public class Employee {
     private Integer id;
     private String username;
--- src/main/java/com/tms/SpringStart.java
+++ src/main/java/com/tms/SpringStart.java
@@ -1,11 +1,13 @@
 package com.tms;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan("com.tms")    //с какой папки начинать вычитывать все классы
 public class SpringStart {
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("spring-settings.xml");
+        ApplicationContext context = new AnnotationConfigApplicationContext(SpringStart.class);
         Employee employee = context.getBean("employee-bean", Employee.class);
 
         System.out.println(employee);
